1eb63126de0270ff89b1225482a3ca516414cd4b
Iago <iagofernandez@prendeluz.com>
2024-09-02 14:53:47 +0200

    WIP Update models, create stock deficit model and base repo tests updated

--- internal/models/order.go
+++ internal/models/order.go
@@ -2,12 +2,21 @@ package models
 
 import "time"
 
+type OrderStatus string
+
+const (
+	InProgress OrderStatus = "in progress"
+	Received   OrderStatus = "received"
+	Completed  OrderStatus = "completed"
+	Canceled   OrderStatus = "canceled"
+)
+
 type Order struct {
 	ID           uint64 `gorm:"primaryKey;autoIncrement"`
 	Orden_compra string `gorm:"primaryKey;size:255;not null"`
 	CreatedAt    time.Time
-	Filename     string `gorm:"size:255; not null"`
-	Status       string `gorm:"size:255;not null; default:'Received'"`
+	Filename     string      `gorm:"size:255; not null"`
+	Status       OrderStatus `gorm:"size:255;not null; type:enum('in progress','received','canceled','completed');default:'received'"`
 }
 
 func (Order) TableName() string {
