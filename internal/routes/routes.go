76b2476111072fbe7aaef536a42dbbcf967a1388
bruno <brunofraga@prendeluz.com>
2024-09-03 11:32:16 +0200

    feat(): cors error in local host solved

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,11 +3,25 @@ package routes
 import (
 	"prendeluz/erp/internal/controllers"
 	"prendeluz/erp/internal/middlewares"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine) {
+	router.Use(cors.New(cors.Config{
+		// Permitir solicitudes desde http://localhost:3000
+		AllowOrigins: []string{"http://localhost:3000"},
+		// Permitir métodos HTTP
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		// Permitir encabezados específicos
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		// Permitir enviar cookies en las solicitudes
+		AllowCredentials: true,
+		// Definir el tiempo de caché para la respuesta preflight
+		MaxAge: 12 * time.Hour,
+	}))
 	router.POST("/login", controllers.Login)
 
 	orderRoutes := router.Group("/order").Use(middlewares.Auth)
