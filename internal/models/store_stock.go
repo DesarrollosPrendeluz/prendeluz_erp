34d8e7d0f5c1c3309b71f58a03475b38123fad3a
bruno <brunofraga@prendeluz.com>
2024-09-04 12:03:27 +0200

    feat(): store stock model update Sku_parente raleted field

--- internal/models/store_stock.go
+++ internal/models/store_stock.go
@@ -3,7 +3,7 @@ package models
 type StoreStock struct {
 	ID         uint64 `gorm:"primaryKey;autoIncrement"`
 	StoreID    uint64 `gorm:"column:id_store;primaryKey;not null"`
-	SKU_Parent string `gorm:"column:parent_sku;primaryKey;not null"`
+	SKU_Parent string `gorm:"column:parent_main_sku;primaryKey;not null"`
 	Amount     int64
 
 	Item  Item  `gorm:"foreignKey:SKU_Parent;references:main_sku;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
