c7ed6ebcda210ca389c0b33bc44db104280d5c7b
bruno <brunofraga@prendeluz.com>
2024-09-04 12:04:55 +0200

    feat(): update order item model references to field and table

--- internal/models/order_item.go
+++ internal/models/order_item.go
@@ -2,14 +2,14 @@ package models
 
 type OrderItem struct {
 	ID      uint64 `gorm:"autoIncrement"`
-	OrderID uint64 `gorm:"column:id_pedido;primaryKey;not null"`
-	ItemID  uint64 `gorm:"column:id_item;primaryKey;not null"`
-	Amount  int64  `gorm:"column:cantidad;not null"`
+	OrderID uint64 `gorm:"column:order_id;primaryKey;not null"`
+	ItemID  uint64 `gorm:"column:item_id;primaryKey;not null"`
+	Amount  int64  `gorm:"column:quantity;not null"`
 
 	Order Order `gorm:"foreignKey:OrderID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 	Item  Item  `gorm:"foreignKey:ItemID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 }
 
 func (OrderItem) TableName() string {
-	return "pedidos_items"
+	return "order_lines"
 }
