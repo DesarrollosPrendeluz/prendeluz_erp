4b4ab2a6799e46e80e975931708f48be3f0a6c83
brunfrag <brfraga99@gmail.com>
2024-10-28 10:25:06 +0100

    feat(): return deficit by supplier

--- internal/controllers/stock_deficit.go
+++ internal/controllers/stock_deficit.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"prendeluz/erp/internal/db"
+	"prendeluz/erp/internal/models"
 	"prendeluz/erp/internal/repositories/stockdeficitrepo"
 	"strconv"
 
@@ -11,12 +12,19 @@ import (
 
 func GetStockDeficit(c *gin.Context) {
 	store, _ := strconv.Atoi(c.DefaultQuery("store_id", "1"))
+	supplier, _ := strconv.Atoi(c.DefaultQuery("supplier", "0"))
 	page, _ := strconv.Atoi(c.DefaultQuery("page", "0"))
 	pageSize, _ := strconv.Atoi(c.DefaultQuery("page_size", "10"))
 
 	repo := stockdeficitrepo.NewStockDeficitRepository(db.DB)
 	recount, _ := repo.CountConditional(store)
-	stockDeficits, _ := repo.GetallByStore(store, pageSize, page)
+	var stockDeficits []models.StockDeficit
+	if supplier == 0 {
+		stockDeficits, _ = repo.GetallByStore(store, pageSize, page)
+	} else {
+		stockDeficits, _ = repo.GetallByStoreAndSupplier(store, supplier, pageSize, page)
+
+	}
 
 	c.IndentedJSON(http.StatusOK, gin.H{"Results": gin.H{"data": stockDeficits, "recount": recount}})
 
--- internal/repositories/stockdeficitrepo/stock_deficit_impl.go
+++ internal/repositories/stockdeficitrepo/stock_deficit_impl.go
@@ -20,6 +20,31 @@ func (repo *StockDeficitImpl) GetallByStore(storeId int, pageSize int, offset in
 	repo.DB.Preload("Item.SupplierItems.Supplier").Where("store_id = ?", storeId).Limit(pageSize).Offset(offset).Find(&models)
 	return models, nil
 }
+func (repo *StockDeficitImpl) GetallByStoreAndSupplier(storeId int, supplier int, pageSize int, offset int) ([]models.StockDeficit, error) {
+	var modelsData []models.StockDeficit
+
+	subQuery := repo.DB.
+		Model(&models.StockDeficit{}).
+		Select("stock_deficits.id").
+		Joins("JOIN items ON items.main_sku = stock_deficits.parent_main_sku").
+		Joins("JOIN supplier_items ON supplier_items.item_id = items.id").
+		//Where("supplier_items.item_id = ?", 2).
+		Where("store_id = ?", storeId).
+		Where("supplier_items.supplier_id = ?", supplier)
+
+	err := repo.DB.
+		//("Item.SupplierItems", "item_id = ?", 2).
+		Preload("Item.SupplierItems.Supplier").
+		Where("id IN (?)", subQuery).
+		Limit(pageSize).
+		Offset(offset).
+		Find(&modelsData).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return modelsData, nil
+}
 
 func (repo *StockDeficitImpl) CountConditional(storeId int) (int64, error) {
 	var count int64
