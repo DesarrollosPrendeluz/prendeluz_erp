6186437f8bddc98d0c8a861d7f1b8915f945f666
brunfrag <brfraga99@gmail.com>
2024-10-31 11:22:13 +0100

    fix(): don´t retrive stock deficit and pending stock at 0

--- internal/repositories/stockdeficitrepo/stock_deficit_impl.go
+++ internal/repositories/stockdeficitrepo/stock_deficit_impl.go
@@ -17,19 +17,26 @@ func NewStockDeficitRepository(db *gorm.DB) *StockDeficitImpl {
 
 func (repo *StockDeficitImpl) GetallByStore(storeId int, pageSize int, offset int) ([]models.StockDeficit, error) {
 	var models []models.StockDeficit
-	repo.DB.Preload("Item.SupplierItems.Supplier").Where("store_id = ?", storeId).Limit(pageSize).Offset(offset).Find(&models)
+	repo.DB.Debug().
+		Preload("Item.SupplierItems.Supplier").
+		Where("store_id = ?", storeId).
+		Where("quantity != 0 OR pending_stock != 0").
+		Limit(pageSize).
+		Offset(offset).
+		Find(&models)
 	return models, nil
 }
 func (repo *StockDeficitImpl) GetallByStoreAndSupplier(storeId int, supplier int, pageSize int, offset int) ([]models.StockDeficit, error) {
 	var modelsData []models.StockDeficit
 
-	subQuery := repo.DB.
+	subQuery := repo.DB.Debug().
 		Model(&models.StockDeficit{}).
 		Select("stock_deficits.id").
 		Joins("JOIN items ON items.main_sku = stock_deficits.parent_main_sku").
 		Joins("JOIN supplier_items ON supplier_items.item_id = items.id").
 		//Where("supplier_items.item_id = ?", 2).
 		Where("store_id = ?", storeId).
+		Where("quantity != 0 OR pending_stock != 0").
 		Where("supplier_items.supplier_id = ?", supplier)
 
 	err := repo.DB.
