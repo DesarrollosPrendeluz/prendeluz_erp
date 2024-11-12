7425aef44b2fd1273fcb838c0c21430bb011112d
bruno <brunofraga@prendeluz.com>
2024-11-12 16:01:53 +0100

    feat(): adapt order model

--- internal/models/order.go
+++ internal/models/order.go
@@ -13,17 +13,15 @@ import "time"
 
 type Order struct {
 	ID            uint64 `gorm:"primaryKey;autoIncrement"`
+	FatherOrderID uint64
 	OrderStatusID uint64
-	OrderTypeID   uint64
 	Code          string `gorm:"primaryKey;size:255;not null"`
-	Filename      string `gorm:"column:file_name;size:255;notnull"`
-
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
 
 	OrderStatus OrderStatus `gorm:"foreignKey:OrderStatusID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
-	OrderType   OrderType   `gorm:"foreignKey:OrderTypeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
-
+	FatherOrder FatherOrder `gorm:"foreignKey:ID;references:FatherOrderID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
+	OrderLines  []OrderItem `gorm:"foreignKey:OrderID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 	// Orden_compra string `gorm:"primaryKey;size:255;not null"`
 	// Filename     string      `gorm:"size:255; not null"`
 	// Status       OrderStatus `gorm:"size:255;not null; type:enum('in progress','received','canceled','completed');default:'received'"`
