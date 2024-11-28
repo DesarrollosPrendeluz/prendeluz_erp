693732794261f0f45175ba07f06b63a63c77ce4d
bruno <brunofraga@prendeluz.com>
2024-11-28 16:26:24 +0100

    feat(): add new endpoints to implement item stock location and store location

---
+++ internal/controllers/item_stock_location_controller.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetItemStockLocation(c *gin.Context) {
+
+}
+
+func PostItemStockLocation(c *gin.Context) {
+
+}
+
+func PatchItemStockLocation(c *gin.Context) {
+
+}
+
+func MovItemStockLocation(c *gin.Context) {
+
+}
---
+++ internal/controllers/store_location_controller.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetStoreLocation(c *gin.Context) {
+
+}
+
+func PostStoreLocation(c *gin.Context) {
+
+}
+
+func PatchStoreLocation(c *gin.Context) {
+
+}
