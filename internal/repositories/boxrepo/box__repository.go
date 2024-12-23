96bd34f4e4bd718b81eeede924ecb3750c01d8a5
bruno <brunofraga@prendeluz.com>
2024-12-23 12:43:23 +0100

    feat(): add GetOrCreateBoxByPalletIdAndNumber method to Box repository for fetching or creating boxes

--- internal/repositories/boxrepo/box__repository.go
+++ internal/repositories/boxrepo/box__repository.go
@@ -7,4 +7,5 @@ import (
 
 type BoxRepo interface {
 	repositories.Repository[models.Box]
+	GetOrCreateBoxByPalletIdAndNumber(palletId int, number int, quantity int) (models.Box, bool, error)
 }
--- internal/repositories/boxrepo/box_impl.go
+++ internal/repositories/boxrepo/box_impl.go
@@ -14,3 +14,28 @@ type BoxImpl struct {
 func NewBoxRepository(db *gorm.DB) *BoxImpl {
 	return &BoxImpl{repositories.NewGORMRepository(db, models.Box{})}
 }
+
+func (repo *BoxImpl) GetOrCreateBoxByPalletIdAndNumber(palletId int, number int, quantity int) (models.Box, bool, error) {
+	var model models.Box
+	flag := false
+	err := repo.DB.
+		Where("pallet_id = ?", palletId).
+		Where("number = ?", number).
+		First(&model).Error
+
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			modelCreate := models.Box{
+				PalletID: uint64(palletId),
+				Number:   number,
+				Label:    "",
+				Quantity: quantity,
+			}
+			repo.DB.Create(&modelCreate)
+			model = modelCreate
+			flag = true
+
+		}
+	}
+	return model, flag, nil
+}
