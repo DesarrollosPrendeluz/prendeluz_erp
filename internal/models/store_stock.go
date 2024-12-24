0bd73323e01d0fa7cefafde391d121cf79bc8279
bruno <brunofraga@prendeluz.com>
2024-12-24 14:41:51 +0100

    feat(): add CreatedAt and UpdatedAt fields to StoreStock model for tracking timestamps

--- internal/models/store_stock.go
+++ internal/models/store_stock.go
@@ -1,11 +1,17 @@
 package models
 
+import (
+	"time"
+)
+
 type StoreStock struct {
 	ID             uint64 `gorm:"primaryKey;autoIncrement"`
 	StoreID        uint64 `gorm:"column:store_id;primaryKey;not null"`
 	SKU_Parent     string `gorm:"column:parent_main_sku;primaryKey;not null"`
 	Amount         int64  `gorm:"column:quantity"`
 	ReservedAmount int64  `gorm:"column:reserved"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 
 	Item  Item  `gorm:"foreignKey:SKU_Parent;references:main_sku;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 	Store Store `gorm:"foreignKey:StoreID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
