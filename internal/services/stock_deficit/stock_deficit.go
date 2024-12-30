6f13d97d113febbdb8ae7f46d62f0b1750838bce
bruno <brunofraga@prendeluz.com>
2024-12-30 09:31:55 +0100

    feat(stock_deficit): implement CalcStockDeficitByItem method for stock deficit calculation

--- internal/services/stock_deficit/stock_deficit.go
+++ internal/services/stock_deficit/stock_deficit.go
@@ -6,4 +6,5 @@ import (
 
 type StockDeficitService interface {
 	SearchBySkuAndEan(filter string, store int, page int, pageSize int) ([]models.StockDeficit, []error)
+	CalcStockDeficitByItem(child_item_id uint64, store_id int64)
 }
