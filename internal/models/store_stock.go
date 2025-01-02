bc0b3ba7a5cdf80633f6c75c1545d2035625dd5d
bruno <brunofraga@prendeluz.com>
2025-01-02 10:52:07 +0100

    fix(models): correct foreign key references in ItemLocation and StoreStock models refactor(stock_service): improve stock Excel download logic and handle location errors

--- internal/models/store_stock.go
+++ internal/models/store_stock.go
@@ -7,13 +7,13 @@ import (
 type StoreStock struct {
 	ID             uint64 `gorm:"primaryKey;autoIncrement"`
 	StoreID        uint64 `gorm:"column:store_id;primaryKey;not null"`
-	SKU_Parent     string `gorm:"column:parent_main_sku;primaryKey;not null"`
+	SKU_Parent     string `gorm:"column:parent_main_sku;not null"`
 	Amount         int64  `gorm:"column:quantity"`
 	ReservedAmount int64  `gorm:"column:reserved"`
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
 
-	Item      Item            `gorm:"foreignKey:SKU_Parent;references:main_sku;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
+	Item      Item            `gorm:"foreignKey:SKU_Parent;references:MainSKU;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 	Locations *[]ItemLocation `gorm:"foreignKey:ItemMainSku;references:SKU_Parent;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 	Store     Store           `gorm:"foreignKey:StoreID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 }
--- internal/services/stock/stock_impl.go
+++ internal/services/stock/stock_impl.go
@@ -32,7 +32,7 @@ func (s *StockServiceImpl) ReturnDownloadStockExcel(store_id int) string {
 	//s.stockRepo.FindByStore(store_id);
 	stocks, _ := s.stockRepo.FindByStoreWithLocations(uint64(store_id))
 	f := excelize.NewFile()
-	f.DeleteSheet("Sheet1")
+
 	// Inicia en la fila 2 para Locations
 
 	// Crear encabezados en la primera fila
@@ -57,23 +57,36 @@ func (s *StockServiceImpl) ReturnDownloadStockExcel(store_id int) string {
 		f.SetCellValue(sheetNameTotals, "B"+strconv.Itoa(totalRow), stock.Item.EAN)
 		f.SetCellValue(sheetNameTotals, "C"+strconv.Itoa(totalRow), stock.ReservedAmount)
 		f.SetCellValue(sheetNameTotals, "D"+strconv.Itoa(totalRow), stock.Amount)
+		if stock.Locations != nil {
+			for locIndex, location := range *stock.Locations {
+				locationCode := "Error en codigo de localización"
+				locStoreCode := uint64(0)
+				if location.StoreLocations != nil {
 
-		for locIndex, location := range *stock.Locations {
-			locRow := totalRow + locIndex
-			locationCode := ""
-			if location.StoreLocations != nil {
-				locationCode = location.StoreLocations.Code
+					locationCode = location.StoreLocations.Code
+					locStoreCode = location.StoreLocations.StoreID
+				} else {
+					fmt.Println("Location error:", location.ID, location.StoreLocationID, stock.SKU_Parent, stock.ID, location.Stock)
+				}
+				if locStoreCode == uint64(store_id) || store_id == 0 {
+					locRow := totalRow + locIndex
+
+					//fmt.Printf("Location: %+v\n", location)
+					f.SetCellValue(sheetNamePartials, "A"+strconv.Itoa(locRow), stock.SKU_Parent)
+					f.SetCellValue(sheetNamePartials, "B"+strconv.Itoa(locRow), stock.Item.EAN)
+
+					f.SetCellValue(sheetNamePartials, "C"+strconv.Itoa(locRow), locationCode)
+					f.SetCellValue(sheetNamePartials, "D"+strconv.Itoa(locRow), location.Stock)
+
+				}
+
 			}
-			fmt.Printf("Location: %+v\n", location)
-			f.SetCellValue(sheetNamePartials, "A"+strconv.Itoa(locRow), stock.SKU_Parent)
-			f.SetCellValue(sheetNamePartials, "B"+strconv.Itoa(locRow), stock.Item.EAN)
-
-			f.SetCellValue(sheetNamePartials, "C"+strconv.Itoa(locRow), locationCode)
-			f.SetCellValue(sheetNamePartials, "D"+strconv.Itoa(locRow), location.Stock)
+		} else {
+			fmt.Println("Location error:", stock.SKU_Parent, stock.ID)
 		}
 
 	}
-
+	f.DeleteSheet("Sheet1")
 	var buf bytes.Buffer
 	if err := f.Write(&buf); err != nil {
 
