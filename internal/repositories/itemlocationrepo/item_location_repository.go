23da4d047f108465a2bd514296aeae6980a89a8c
brunfrag <brfraga99@gmail.com>
2025-01-14 15:19:06 +0100

    feat(): add select method to item location repo

--- internal/repositories/itemlocationrepo/item_location_repository.go
+++ internal/repositories/itemlocationrepo/item_location_repository.go
@@ -10,4 +10,5 @@ type StoreLocationRepo interface {
 	FindByItem(mainSku string, pageSize int, offset int) ([]models.ItemLocation, error)
 	FindByItemsAndStore(mainSku string, store uint64, pageSize int, offset int) ([]models.ItemLocation, error)
 	FindByItemsAndLocation(mainSku string, location uint64) (models.ItemLocation, error)
+	FindByItemAndLocation(mainSku string, locationId uint64) (models.ItemLocation, error)
 }
