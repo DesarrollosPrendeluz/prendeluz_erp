7d606785e240807f81d16a27c1ce9657f6265ba8
bruno <brunofraga@prendeluz.com>
2025-01-16 14:52:57 +0100

    feat: add methods to find order items by item, order, and store

--- internal/repositories/orderitemrepo/order_item_repository.go
+++ internal/repositories/orderitemrepo/order_item_repository.go
@@ -10,8 +10,9 @@ type OrderItemRepo interface {
 	FindByOrder(idOrder uint64) ([]models.OrderItem, error)
 	FindByItem(idPedido uint64) ([]models.OrderItem, error)
 	FindByOrderAndItem(orderIds []uint64, storeId int, itemIds []uint64, offset int, pageSize int) ([]models.OrderItem, int64)
+	FindByItemOrderStore(itemId uint64, orderId uint64, storeId uint64) (models.OrderItem, error)
+	FindByItemAndOrders(orderIds []uint64, itemId uint64, storeId uint64) (models.OrderItem, error)
 	FindByItemsAndOrder(itemIds []uint64, orderId uint64) (models.OrderItem, error)
 	FindByOrderAndStore(idOrder uint64, store_id int) ([]models.OrderItem, error)
 	FindByLineIDWithOrder(lineId []uint64, order string, offset int, pageSize int) ([]models.OrderItem, int64)
-
 }
