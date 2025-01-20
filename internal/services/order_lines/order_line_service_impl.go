1b194abb1900f91a5bb092a55c29216aaff7c6ef
bruno <brunofraga@prendeluz.com>
2025-01-20 14:39:05 +0100

    feat: integrate ErpUpdateOrderLineHistory repository into OrderLineServiceImpl for order line history tracking

--- internal/services/order_lines/order_line_service_impl.go
+++ internal/services/order_lines/order_line_service_impl.go
@@ -7,6 +7,7 @@ import (
 	"prendeluz/erp/internal/dtos"
 	"prendeluz/erp/internal/models"
 	"prendeluz/erp/internal/repositories"
+	"prendeluz/erp/internal/repositories/erpupdateorderlinehistoryrepo"
 	"prendeluz/erp/internal/repositories/itemsrepo"
 	"prendeluz/erp/internal/repositories/orderitemrepo"
 	"prendeluz/erp/internal/repositories/orderrepo"
@@ -23,21 +24,24 @@ type OrderLineServiceImpl struct {
 	orderRepo      orderrepo.OrderRepoImpl
 	orderItemsRepo orderitemrepo.OrderItemRepoImpl
 	//itemRepo       itemsrepo.ItemRepoImpl
-	orderErrorRepo repositories.GORMRepository[models.ErrorOrder]
-	itemsRepo      itemsrepo.ItemRepoImpl
+	erpupdateorderlinehistoryrepo erpupdateorderlinehistoryrepo.ErpUpdateOrderLineHistoryImpl
+	orderErrorRepo                repositories.GORMRepository[models.ErrorOrder]
+	itemsRepo                     itemsrepo.ItemRepoImpl
 }
 
 func NewOrderLineServiceImpl() *OrderLineServiceImpl {
 	orderRepo := *orderrepo.NewOrderRepository(db.DB)
 	errorOrderRepo := *repositories.NewGORMRepository(db.DB, models.ErrorOrder{})
+	erpupdateorderlinehistoryrepo := *erpupdateorderlinehistoryrepo.NewErpUpdateOrderLineHistoryRepository(db.DB)
 	orderItemRepo := *orderitemrepo.NewOrderItemRepository(db.DB)
 	itemsRepo := *itemsrepo.NewItemRepository(db.DB)
 
 	return &OrderLineServiceImpl{
-		orderRepo:      orderRepo,
-		orderItemsRepo: orderItemRepo,
-		orderErrorRepo: errorOrderRepo,
-		itemsRepo:      itemsRepo,
+		orderRepo:                     orderRepo,
+		orderItemsRepo:                orderItemRepo,
+		orderErrorRepo:                errorOrderRepo,
+		erpupdateorderlinehistoryrepo: erpupdateorderlinehistoryrepo,
+		itemsRepo:                     itemsRepo,
 	}
 }
 
@@ -158,6 +162,8 @@ func (s *OrderLineServiceImpl) UpdateOrderLineHandler(
 			*failedIds = append(*failedIds, int(dataItem.Id))
 
 		} else {
+			orderLine, _ := s.orderItemsRepo.FindByID(dataItem.Id)
+			s.erpupdateorderlinehistoryrepo.GenerateOrderLineHistory(*orderLine, user.UserId, 1)
 
 			updateOrderLine(c, dataItem, errorList, callback)
 		}
