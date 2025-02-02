bec4691d98de01afa0178a1e48f5608e79f9c991
bruno <brunofraga@prendeluz.com>
2025-02-02 19:58:40 +0100

    refactor: ItemStockLocation controller

--- internal/controllers/item_stock_location_controller.go
+++ internal/controllers/item_stock_location_controller.go
@@ -5,12 +5,8 @@ import (
 	"strconv"
 
 	"errors"
-	"prendeluz/erp/internal/db"
 	"prendeluz/erp/internal/dtos"
 	"prendeluz/erp/internal/models"
-	"prendeluz/erp/internal/repositories/itemlocationrepo"
-	"prendeluz/erp/internal/repositories/storelocationrepo"
-	"prendeluz/erp/internal/repositories/storestockrepo"
 	ItemStockLocationService "prendeluz/erp/internal/services/item_stock_locations"
 
 	"github.com/gin-gonic/gin"
@@ -19,35 +15,15 @@ import (
 func GetItemStockLocation(c *gin.Context) {
 	var err error
 	var data []models.ItemLocation
-	var datum *models.ItemLocation
 	var recount int64
 
-	repo := itemlocationrepo.NewInItemLocationRepository(db.DB)
 	main_sku := c.Query("main_sku")
 	store_id, _ := strconv.Atoi(c.DefaultQuery("store_id", "0"))
 	storeLocation, _ := strconv.Atoi(c.DefaultQuery("item_stock_location", "0"))
 	page, _ := strconv.Atoi(c.DefaultQuery("page", "0"))
 	pageSize, _ := strconv.Atoi(c.DefaultQuery("page_size", "10"))
 
-	if main_sku != "" && store_id != 0 {
-		data, err = repo.FindByItemsAndStore(main_sku, uint64(store_id), pageSize, page)
-
-	} else if main_sku != "" {
-		data, err = repo.FindByItem(main_sku, pageSize, page)
-	} else {
-		if storeLocation != 0 {
-			datum, err = repo.FindByID(uint64(storeLocation))
-			if datum != nil {
-				data = append(data, *datum)
-			}
-			recount = 1
-		} else {
-			data, err = repo.FindAll(pageSize, page)
-			recount, _ = repo.CountAll()
-
-		}
-
-	}
+	data, recount, err = ItemStockLocationService.NewItemStockLocationService().GetItemStockLocation(main_sku, store_id, storeLocation, page, pageSize)
 
 	if err != nil {
 		c.IndentedJSON(http.StatusOK, gin.H{"Error": gin.H{"err": err}})
@@ -60,7 +36,7 @@ func GetItemStockLocation(c *gin.Context) {
 func DropItemStockLocation(c *gin.Context) {
 	location_id, _ := strconv.Atoi(c.DefaultQuery("location_id", "0"))
 	var error error
-	error = errors.New("Se ha enviado id = 0")
+	error = errors.New("se ha enviado id = 0")
 	if location_id != 0 {
 		error = ItemStockLocationService.NewItemStockLocationService().DropItemLocation(uint64(location_id))
 	}
@@ -84,18 +60,7 @@ func PostItemStockLocation(c *gin.Context) {
 		return
 	}
 
-	// Acceder a los valores del cuerpo
-	for _, dataItem := range requestBody.Data {
-
-		repo := itemlocationrepo.NewInItemLocationRepository(db.DB)
-		model := models.ItemLocation{
-			ItemMainSku:     dataItem.ItemMainSku,
-			StoreLocationID: dataItem.StoreLocationID,
-			Stock:           dataItem.Stock,
-		}
-		repo.Create(&model)
-		idArray = append(idArray, model.ID)
-	}
+	idArray = ItemStockLocationService.NewItemStockLocationService().PostItemStockLocation(requestBody)
 	c.JSON(http.StatusAccepted, gin.H{"Results": gin.H{"Ok": "Registers are created", "CreatedIds": idArray}})
 
 }
@@ -111,29 +76,8 @@ func PatchItemStockLocation(c *gin.Context) {
 		return
 	}
 
-	// Acceder a los valores del cuerpo
-	repo := itemlocationrepo.NewInItemLocationRepository(db.DB)
-	for _, requestObject := range requestBody.Data {
-		model, err := repo.FindByID(requestObject.Id)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		if requestObject.ItemMainSku != nil {
-			model.ItemMainSku = *requestObject.ItemMainSku
-		}
-		if requestObject.StoreLocationID != nil {
-			model.StoreLocationID = *requestObject.StoreLocationID
-		}
-		if requestObject.Stock != nil {
-			model.Stock = *requestObject.Stock
-		}
-		error := repo.Update(model)
-		if error != nil {
-			errorList = append(errorList, error)
-		}
+	errorList = ItemStockLocationService.NewItemStockLocationService().PatchItemStockLocation(requestBody)
 
-	}
 	c.JSON(http.StatusAccepted, gin.H{"Results": gin.H{"Ok": "Stock locations are updated", "Errors": errorList}})
 
 }
@@ -150,36 +94,8 @@ func StockChanges(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	errorList = ItemStockLocationService.NewItemStockLocationService().StockChanges(requestBody)
 
-	// Acceder a los valores del cuerpo
-	repo := itemlocationrepo.NewInItemLocationRepository(db.DB)
-	repoLoc := storelocationrepo.NewStoreLocationRepository(db.DB)
-	repoStock := storestockrepo.NewStoreStockRepository(db.DB)
-	for _, requestObject := range requestBody.Data {
-		model, err := repo.FindByID(requestObject.Id)
-		loc, err1 := repoLoc.FindByID(model.StoreLocationID)
-		stock, err2 := repoStock.FindByItemAndStore(model.ItemMainSku, strconv.FormatUint(loc.StoreID, 10))
-		if err != nil || err1 != nil || err2 != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		stock.Amount = ((stock.Amount - int64(model.Stock)) + int64(requestObject.Stock))
-		model.Stock = requestObject.Stock
-		if requestObject.Stock >= 0 {
-			error := repo.Update(model)
-			error2 := repoStock.Update(&stock)
-			if error != nil && error2 != nil {
-				errorList = append(errorList, error)
-				errorList = append(errorList, error2)
-			}
-
-		} else {
-
-			errorList = append(errorList, errors.New("Stock can't be negative"))
-		}
-
-	}
 	if len(errorList) > 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"Results": gin.H{"error": errorList}})
 		return
@@ -192,33 +108,6 @@ func StockMovements(c *gin.Context) {
 
 	var requestBody dtos.ItemStockLocationStockMovementRequest
 	var errorList []error
-	repo := itemlocationrepo.NewInItemLocationRepository(db.DB)
-	repoLoc := storelocationrepo.NewStoreLocationRepository(db.DB)
-	repoStock := storestockrepo.NewStoreStockRepository(db.DB)
-
-	stockMov := func(sku string, location uint64, stockVariant int64) error {
-		model, err := repo.FindByItemsAndLocation(sku, location)
-		loc, err1 := repoLoc.FindByID(model.StoreLocationID)
-		stock, err2 := repoStock.FindByItemAndStore(model.ItemMainSku, strconv.FormatUint(loc.StoreID, 10))
-		if err != nil || err1 != nil || err2 != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return err
-		}
-
-		stock.Amount = (stock.Amount + stockVariant)
-		model.Stock = model.Stock + int(stockVariant)
-		if model.Stock < 0 {
-			return errors.New("Stock can't be negative")
-
-		}
-		error := repo.Update(&model)
-		error2 := repoStock.Update(&stock)
-		if error != nil || error2 != nil {
-			errorList = append(errorList, error)
-			errorList = append(errorList, error2)
-		}
-		return nil
-	}
 
 	// Intentar bindear los datos del cuerpo de la request al struct
 	if err := c.ShouldBindJSON(&requestBody); err != nil {
@@ -226,25 +115,8 @@ func StockMovements(c *gin.Context) {
 		return
 	}
 
-	// Acceder a los valores del cuerpo
+	errorList = ItemStockLocationService.NewItemStockLocationService().StockMovements(requestBody)
 
-	for _, requestObject := range requestBody.Data {
-		if requestObject.StoreLocationID1 != requestObject.StoreLocationID2 {
-			errMov := stockMov(requestObject.MainSku, requestObject.StoreLocationID1, -int64(requestObject.Stock))
-			if errMov == nil {
-				errMov2 := stockMov(requestObject.MainSku, requestObject.StoreLocationID2, int64(requestObject.Stock))
-				if errMov2 != nil {
-					errorList = append(errorList, errMov2)
-				}
-			} else {
-				errorList = append(errorList, errMov)
-			}
-
-		} else {
-			errorList = append(errorList, errors.New("The locations are the same"))
-		}
-
-	}
 	if len(errorList) > 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"Results": gin.H{"error": errorList}})
 		return
--- internal/services/item_stock_locations/item_stock_locations.go
+++ internal/services/item_stock_locations/item_stock_locations.go
@@ -1,5 +1,15 @@
 package services
 
+import (
+	"prendeluz/erp/internal/dtos"
+	"prendeluz/erp/internal/models"
+)
+
 type ItemStockLocationService interface {
 	DropItemLocation(locationId uint64) error
+	GetItemStockLocation(main_sku string, store_id int, storeLocation int, page int, pageSize int) ([]models.ItemLocation, int64, error)
+	PostItemStockLocation(requestBody dtos.ItemStockLocationCreateReq) []uint64
+	PatchItemStockLocation(requestBody dtos.ItemStockLocationUpdateReq) []error
+	StockChanges(requestBody dtos.ItemStockLocationStockChangeRequest) []error
+	StockMovements(requestBody dtos.ItemStockLocationStockMovementRequest) []error
 }
