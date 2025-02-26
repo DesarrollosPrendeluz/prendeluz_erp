f615d303efaaa7663e3eb7d1452bcc881353de3e
bruno <brunofraga@prendeluz.com>
2025-02-26 15:47:14 +0100

    feat: implement FindBySupplierOrder method and update related order processing logic

--- internal/repositories/suppliersoldorderrelationrepo/supplier_sold_order_impl.go
+++ internal/repositories/suppliersoldorderrelationrepo/supplier_sold_order_impl.go
@@ -14,3 +14,12 @@ type SupplierSoldOrderRelationImpl struct {
 func NewSupplierSoldOrderRelationRepository(db *gorm.DB) *SupplierSoldOrderRelationImpl {
 	return &SupplierSoldOrderRelationImpl{repositories.NewGORMRepository(db, models.SupplierSoldOrderRelation{})}
 }
+
+func (repo *SupplierSoldOrderRelationImpl) FindBySupplierOrder(id uint64) (models.SupplierSoldOrderRelation, error) {
+	var relation models.SupplierSoldOrderRelation
+
+	result := repo.DB.Debug().Where("supplier_father_order_id = ?", id).First(&relation)
+
+	return relation, result.Error
+
+}
